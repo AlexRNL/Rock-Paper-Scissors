389b38e23a8c14dd9e652696fcd206bdfed487ce
Alex Barféty <alexbarfety@free.fr>
2023-11-11 00:19:07 +0100

    Move source files to src folder.

---
+++ src/main.h
@@ -0,0 +1,25 @@
+/************************************************************/
+/*                         main.h                           */
+/*   Ce fichier contient inclus toutes les biblioth�ques    */
+/* standard dont on aura besoin lors du jeu ainsi que les   */
+/* prototypes des fonctions.                                */
+/*   Auteur : Alex Barféty                                  */
+/*   Date de création : 27/12/2007                          */
+/*   Date de modification : 10/04/2008                      */
+/*                                                          */
+/************************************************************/
+
+#ifndef DEF_MAINH
+#define DEF_MAINH
+
+//       BIBLIOTHEQUES STANDARD
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//       PROTOTYPES
+char int_art (void) ;
+void print_element (char element, char elt[12]) ;
+
+#endif
